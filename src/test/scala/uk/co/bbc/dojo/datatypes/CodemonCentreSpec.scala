4e07cb4d01e9f310cc0ff5bd2ea3ffaa33efc7f0
Russell Broughton <russell.broughton@bbc.co.uk>
2017-05-08 18:46:58 +0100

    Bulked out Centre Specs; added .gitignore and build.sbt

--- src/test/scala/uk/co/bbc/dojo/datatypes/CodemonCentreSpec.scala
+++ src/test/scala/uk/co/bbc/dojo/datatypes/CodemonCentreSpec.scala
@@ -1,18 +1,39 @@
 package uk.co.bbc.dojo.datatypes
 
 import org.scalatest.{FunSpec, Matchers}
+import uk.co.bbc.dojo.dataypes._
 
-//TODO: Can take Codemon or Codeballs
 class CodemonCentreSpec extends FunSpec with Matchers {
-  describe("The dystopian mass processing facility should") {
-    it("be able to contain no Codemon") {
+  describe("#4 - The dystopian mass processing facility should") {
+    it("be able to contain no Codeballs") {
+      val centre = CodemonCentre()
 
+      centre.codeBalls shouldBe Seq.empty[CodeBall]
     }
 
-    it("do nothing when we apply a functor over an empty facility")
+    it("do nothing when we apply a functor over an empty facility") {
+      val centre = CodemonCentre()
 
-    it("be able to contain and retrieve a number of Codemon")
+      val evolvedCentre = centre.map(codeBall => codeBall.map(codemon => codemon.evolve))
 
-    it("be able to operate over all of the Codemon")
+      evolvedCentre shouldBe centre
+    }
+
+    it("be able to contain and retrieve a number of Codemon") {
+      val convicts = List(OccupiedCodeball(RaabyChu), OccupiedCodeball(Rusa))
+
+      val centre = CodemonCentre(convicts: _*)
+
+      centre.codeBalls shouldBe convicts
+    }
+
+    it("be able to mass-process the imprisoned Codemon") {
+      val convicts = List(OccupiedCodeball(RaabyChu), OccupiedCodeball(Rusa))
+
+      val centre = CodemonCentre(convicts: _*)
+      val evolvedCentre = centre.map(codeBall => codeBall.map(codemon => codemon.evolve))
+
+      evolvedCentre.codeBalls shouldBe List(OccupiedCodeball(RaabyChu), OccupiedCodeball(Sikachu))
+    }
   }
 }
