564897475a5e17ef8bcea587a6a61aa4e0d0b1e8
Russell Broughton <russell.broughton@bbc.co.uk>
2017-05-21 15:16:19 +0100

    Added flatmap behaviour (and tests) for CodeBox

--- src/main/scala/uk/co/bbc/dojo/dataypes/CodeBox.scala
+++ src/main/scala/uk/co/bbc/dojo/dataypes/CodeBox.scala
@@ -1,6 +1,8 @@
 package uk.co.bbc.dojo.dataypes
 
 object CodeBox {
+  def identity[A](codeBox: CodeBox[A]): CodeBox[A] = codeBox
+
   def flatten[A](codebox: CodeBox[CodeBox[A]]): CodeBox[A] = {
     val newContents = go(codebox.contents)
     CodeBox(newContents)
@@ -15,4 +17,5 @@ object CodeBox {
 // Now genericised the functor to allow changes between types
 case class CodeBox[A](contents: List[A]) {
   def map[B](f: A => B): CodeBox[B] = CodeBox[B](contents.map(f))
+  def flatMap[B](f: A => CodeBox[B]): CodeBox[B] = CodeBox.flatten(map(f))
 }
--- src/test/scala/uk/co/bbc/dojo/datatypes/CodeBoxSpec.scala
+++ src/test/scala/uk/co/bbc/dojo/datatypes/CodeBoxSpec.scala
@@ -27,12 +27,20 @@ class CodeBoxSpec extends CodemonBaseSpec {
 
       codeBox.map(_.chooseYou) shouldBe CodeBox(List(RaabyChu, Rusa))
     }
+
+    it("should be able to facilitate mass Codemon evolution") {
+      val codemon = List(RaabyChu, Rusa)
+      val codeBox = CodeBox(codemon)
+
+      def evolveAll(codeBox: CodeBox[Codemon]): CodeBox[Codemon] = codeBox.map(Codemon.evolve)
+
+      evolveAll(codeBox) shouldBe CodeBox(List(RaabyChu, Sikachu))
+    }
   }
 
   describe("#6 - The Exciting new flatten functionality that our CodeBox supports") {
     it("should empty out a single inner box of Codemon") {
       val codemon = List[Codemon](RaabyChu, Sikachu)
-
       val codeBox: CodeBox[CodeBox[Codemon]] = CodeBox(List(CodeBox(codemon)))
 
       CodeBox.flatten(codeBox) shouldBe CodeBox(codemon)
@@ -46,6 +54,55 @@ class CodeBoxSpec extends CodemonBaseSpec {
       val packedOuterCodebox = CodeBox(List(CodeBox(someCodemon), CodeBox(anotherCodemon), CodeBox(aFinalFewCodemon)))
 
       CodeBox.flatten(packedOuterCodebox) shouldBe CodeBox(someCodemon ++ anotherCodemon ++ aFinalFewCodemon)
+    }
+  }
+
+  //TODO: Restriction on Flatmap?
+  //TODO: Make writing these tests the exercise
+  describe("#7 - The killer app, flatmap, should") {
+    it("act as flaten if we pass the identity function") {
+      val codemon = List[Codemon](RaabyChu, Sikachu)
+      val codeBox: CodeBox[CodeBox[Codemon]] = CodeBox(List(CodeBox(codemon)))
+
+      codeBox.flatMap(CodeBox.identity) shouldBe CodeBox.flatten(codeBox)
+    }
+
+    it("act as map if we wrap the function in the unit function") {
+      val codemon = List[Codemon](RaabyChu, Rusa, Sikachu)
+      val codeBox: CodeBox[CodeBox[Codemon]] = CodeBox(List(CodeBox(codemon)))
+
+      def mapWrap[A,B](f: A => B): A => CodeBox[B] = (x: A) => CodeBox(List(f(x)))
+      def evolveAll(codeBox: CodeBox[Codemon]): CodeBox[Codemon] = codeBox.map(Codemon.evolve)
+      def combinedFunction: (CodeBox[Codemon]) => CodeBox[CodeBox[Codemon]] = mapWrap(evolveAll)
+
+      codeBox.flatMap(combinedFunction) shouldBe codeBox.map(evolveAll)
+    }
+
+    it("allow us to throw away all but the Sikachus") {
+      val codemon = List[Codemon](Sikachu, Sikachu, RaabyChu, Rusa, Sikachu)
+      val codeBox: CodeBox[Codemon] = CodeBox(codemon)
+
+      def keepSickachus(codemon: Codemon): CodeBox[Codemon] = codemon match {
+        case Sikachu => CodeBox(List(Sikachu))
+        case _ => CodeBox(List())
+      }
+
+      codeBox.flatMap(keepSickachus) shouldBe CodeBox(List[Codemon](Sikachu, Sikachu, Sikachu))
+    }
+
+    it("Allow us replace each RaabyChu with the equivalant Thousand Rusas") {
+      val codemon = List[Codemon](RaabyChu, Rusa, Sikachu, RaabyChu, Rusa)
+      val codeBox: CodeBox[Codemon] = CodeBox(codemon)
+
+      val aThousandRusas = List.fill(1000)(Rusa)
+      def keepSickachus(codemon: Codemon): CodeBox[Codemon] = codemon match {
+        case RaabyChu => CodeBox(aThousandRusas)
+        case notARusa => CodeBox(List(notARusa))
+      }
+
+      val expectedList = aThousandRusas ++ List(Rusa) ++ List(Sikachu) ++ aThousandRusas ++ List(Rusa)
+
+      codeBox.flatMap(keepSickachus) shouldBe CodeBox(expectedList)
     }
   }
 }
