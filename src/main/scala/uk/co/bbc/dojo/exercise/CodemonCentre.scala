07a9a05ad32c6218d510fe8de28be02b13ac1ca9
Russell Broughton <russell.broughton@bbc.co.uk>
2017-06-25 12:58:20 +0100

    Removed unused concept after exercise rework

--- src/main/scala/uk/co/bbc/dojo/exercise/CodemonCentre.scala
+++
@@ -1,6 +0,0 @@
-package uk.co.bbc.dojo.exercise
-
-// More useful functor capabilities
-case class CodemonCentre(codeBalls: CodeBall*) {
-  def map(f: CodeBall => CodeBall): CodemonCentre = CodemonCentre(codeBalls.map(codeball => f(codeball)): _*)
-}
