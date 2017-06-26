d6cffd68f89c5558e0bf4dfed6b7b36a86262e54
Russell Broughton <russell.broughton@bbc.co.uk>
2017-06-26 11:53:49 +0100

    Added improved flatMap example exercise

---
+++ src/main/scala/uk/co/bbc/dojo/exercise/ImprovedWildCodemonCapture.scala
@@ -0,0 +1,21 @@
+package uk.co.bbc.dojo.exercise
+
+import uk.co.bbc.dojo.exercise.WildCodemonCapture.{baitTrap, fastEvolveCodemon, throwCodeball}
+
+object ImprovedWildCodemonCapture {
+  //TODO: Probably get rid of this and use a single ball type.
+  implicit def advavancedCodeballToMasterCodeball[A](advancedCodeball: AdvancedCodeball[A]): MasterCodeball[A] = advancedCodeball match {
+    case EmptyAdvancedBall => EmptyMasterBall
+    case OccupiedAdvancedBall(contents) => OccupiedMasterBall(contents)
+  }
+
+  def mapViaFlatmap[A, B](masterCodeball: MasterCodeball[A])(f: A => B): MasterCodeball[B] =
+    MasterCodeball.flatMap(masterCodeball)((x: A) => MasterCodeball.pure(f(x)))
+
+  def captureLifecyclewithMonads(randomFunction: () => Boolean): MasterCodeball[Codemon] =
+    MasterCodeball.flatMap(throwCodeball(randomFunction)) { codemonWeCaught: Codemon =>
+      MasterCodeball.flatMap(baitTrap(codemonWeCaught)) { trappedNewCodemon: Codemon =>
+        fastEvolveCodemon(randomFunction)(trappedNewCodemon)
+      }
+    }
+}
---
+++ src/test/scala/uk/co/bbc/dojo/exercise/ImprovedWildCodemonCaptureSpec.scala
@@ -0,0 +1,17 @@
+package uk.co.bbc.dojo.exercise
+
+import uk.co.bbc.dojo.exercise.housekeeping.CodemonBaseSpec
+
+class ImprovedWildCodemonCaptureSpec extends CodemonBaseSpec {
+  describe("#12 - Now with the unlimited power of the Monad we can") {
+    it("write map in terms of flatMap and pure") {
+      ImprovedWildCodemonCapture.mapViaFlatmap(EmptyMasterBall)(x => "cat") shouldBe EmptyMasterBall
+      ImprovedWildCodemonCapture.mapViaFlatmap(OccupiedMasterBall(100))(x => x + 7) shouldBe OccupiedMasterBall(107)
+      ImprovedWildCodemonCapture.mapViaFlatmap(OccupiedMasterBall("Here"))(_ => "Gone") shouldBe OccupiedMasterBall("Gone")
+    }
+
+    it("sequence theImprovedWildCodemonCapture capture operations without causing crazy levels of nesting") {
+      ImprovedWildCodemonCapture.captureLifecyclewithMonads(() => true) shouldBe OccupiedMasterBall(RaabyChu)
+    }
+  }
+}
